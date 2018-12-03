91620f1def1918b7dfe68eae23d7a7c648e82b4a
Melissa Saw <melissasaw95@hotmail.com>
2018-12-03 16:02:45 +0800

    Created show page for pokemons. Upon creationg, redirects to show page

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,26 +1,31 @@
 class PokemonsController < ApplicationController
 
-def new
+	def new
 
-end
-
-def create
-	byebug
-	# Receives a json object
-	# Convert json object back into an array
-	pokemon_array = JSON.parse(params[:jsonData])
-	# create new pokemon for user
-	pokemon = current_user.pokemons.new(pokemon_array[0])
-
-	if pokemon.save
-		redirect_to root_path
-		
-		# deduct money from user
-	else
-		redirect_to root_path
-		# do not deduct coins from user
 	end
 
-end
+	def create
+		# Receives a json object
+		# Convert json object back into an array
+		pokemon_array = JSON.parse(params[:jsonData])
+		# create new pokemon for user
+		pokemon = current_user.pokemons.new(pokemon_array[0])
+
+		if pokemon.save
+			redirect_to pokemon_path(pokemon)
+
+			# deduct money from user
+		else
+			redirect_to root_path
+			flash[:info] = "Generator Failed. Please try again"
+			# do not deduct coins from user
+		end
+
+	end
+
+	def show
+		byebug
+		@pokemon = Pokemon.find_by(id:params[:id])
+	end
 
 end
