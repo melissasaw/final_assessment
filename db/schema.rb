d17351d4a75d15c31f7753a9c342f111756bc0c0
Melissa Saw <melissasaw95@hotmail.com>
2018-12-03 14:54:13 +0800

    Made pokemon model and controller. Added foreign key to pokemon.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_083637) do
+ActiveRecord::Schema.define(version: 2018_12_03_064929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string "name"
+    t.integer "national_id"
+    t.float "height"
+    t.float "weight"
+    t.string "moves"
+    t.integer "rarity"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pokemons_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
