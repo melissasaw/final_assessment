8e91ec6ea999b6a0b2ca2cfbc97c10ba8120b468
Melissa Saw <melissasaw95@hotmail.com>
2018-12-04 14:36:06 +0800

    Completed edit section for pokemon, able to change name of your pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  patch '/pokemon/:id' => "users#update"
   resources :users
   resources :pokemons
   root "welcome#index"
