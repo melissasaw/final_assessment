39dd45053ba55becbf75aa7140645b32fd9c9906
Melissa Saw <melissasaw95@hotmail.com>
2018-12-05 15:50:38 +0800

    Added validations to user sign up before beginning test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
 	has_many :pokemons
 	has_many :authentications, dependent: :destroy
 
+	validates :first_name,:last_name,:email,:password, presence: true
+	validates :email, uniqueness: true
+	validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Invalid format.Please try again!" }
+
+
 	def self.create_with_auth_and_hash(authentication, auth_hash)
 	   user = self.create!(
 	     first_name: auth_hash["info"]["name"],
